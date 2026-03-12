532925b0e4e68e016f4e0840ae61a2318d67a915
rotbotd <246409019+rotbotd@users.noreply.github.com>
2026-03-11 20:02:23 -0400

    add PrintWindow Avalonia port
    Co-authored-by: romdotdog <70765447+romdotdog@users.noreply.github.com>

--- ShareNux.Preview/Program.cs
+++ ShareNux.Preview/Program.cs
@@ -20,7 +20,7 @@ class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new PrintTextWindow();
+            desktop.MainWindow = new PrintWindow();
         }
         base.OnFrameworkInitializationCompleted();
     }
